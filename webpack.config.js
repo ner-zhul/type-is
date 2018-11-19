f9b7d5142cf1ceecff28a6fb0832651ed189f1a8
MhcII <snail975@gmail.com>
2018-11-19 14:38:29 +0800

    添加 webpack 打包

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'type-is.js',
+        path: path.resolve(__dirname, 'lib'),
+    },
+    plugins: [new CleanWebpackPlugin(['dist'])],
+    module: {
+        rules: [
+            { test: /\.m?js$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader' },
+        ],
+    },
+};
