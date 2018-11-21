6a4c028e93996cf8ae1f7fe97616c456efdd7753
MhcII <snail975@gmail.com>
2018-11-21 09:44:23 +0800

    安装 yaml 插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import yaml from 'rollup-plugin-yaml';
 import pkg from './package.json';
 
 export default {
   input: 'src/main.js',
   output: { name: 'typeIs', file: pkg.main, format: 'es' },
+  plugins: [yaml()],
 };
