4c28514a695f118bc1824351c21a9b81035003c0
MhcII <snail975@gmail.com>
2018-12-07 09:35:19 +0800

    修改 rollup 配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import yaml from 'rollup-plugin-yaml';
+import cleaner from 'rollup-plugin-cleaner';
 import pkg from './package.json';
+/* eslint-enable import/no-extraneous-dependencies */
 
-export default [
-  {
-    input: 'src/main.js',
-    output: { name: 'typeIs', file: pkg.main, format: 'umd' },
-    plugins: [yaml()],
-  },
-  {
-    input: 'src/main.js',
-    output: { name: 'typeIs', file: pkg.module, format: 'es' },
-    plugins: [yaml()],
-  }];
+export default {
+  input: 'src/main.js',
+  output: [
+    { name: 'typeIs', file: pkg.module, format: 'es' },
+    { name: 'typeIs', file: pkg.browser, format: 'umd' },
+    { name: 'typeIs', file: pkg.main, format: 'cjs' },
+  ],
+  plugins: [yaml(), cleaner({ targets: ['./dist/'] })],
+};
